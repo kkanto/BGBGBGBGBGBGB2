ab3d7d53848eb7dd7e81462d144231d6b43ca4e2
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-12 22:15:13 +0200

    Create maven project

---
+++ src/main/java/de/allianz/BGBGBGBGBGBGB2/Bgbgbgbgbgbgb2Application.java
@@ -0,0 +1,13 @@
+package de.allianz.BGBGBGBGBGBGB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bgbgbgbgbgbgb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bgbgbgbgbgbgb2Application.class, args);
+	}
+
+}
